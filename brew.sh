e89b9a919b46fddb248348023b399722f2818cfd
Mien <mien@Miens-Mac-Pro.local>
2014-10-30 12:05:03 +0100

    added preztofiles

--- brew.sh
+++ brew.sh
@@ -5,7 +5,16 @@
 # Ask for the administrator password upfront.
 sudo -v
 
-# Keep-alive: update existing `sudo` time stamp until `.osx` has finished.
+if [[ ! -x /usr/local/bin/brew ]]; then
+    echo "Info   | Install   | homebrew"
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+    # Modify the PATH
+    export PATH=/usr/local/bin:$PATH
+fi
+
+
+# Keep-alive: Keep `sudo` time stamp until finished.
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
 # Make sure we’re using the latest Homebrew.
@@ -25,71 +34,68 @@ brew install moreutils
 brew install findutils
 # Install GNU `sed`, overwriting the built-in `sed`.
 brew install gnu-sed --default-names
-# Install Bash 4.
-# Note: don’t forget to add `/usr/local/bin/bash` to `/etc/shells` before
-# running `chsh`.
-brew install bash
-brew install bash-completion
+
+brew install zsh
+
+# Download and install python
+if [[ ! -x /usr/local/bin/python ]]; then
+    echo "Info   | Install   | python"
+    brew install python --framework --with-brewed-openssl
+fi
 
 # Install `wget` with IRI support.
 brew install wget --enable-iri
 
-# Install RingoJS and Narwhal.
-# Note that the order in which these are installed is important;
-# see http://git.io/brew-narwhal-ringo.
-brew install ringojs
-brew install narwhal
-
 # Install more recent versions of some OS X tools.
-brew install vim --override-system-vi
+brew install macvim --override-system-vim
 brew install homebrew/dupes/grep
-brew install homebrew/dupes/screen
 brew install homebrew/php/php55 --with-gmp
-
-# Install some CTF tools; see https://github.com/ctfs/write-ups.
-brew install bfg
-brew install binutils
-brew install binwalk
-brew install cifer
-brew install dex2jar
-brew install dns2tcp
-brew install fcrackzip
-brew install foremost
-brew install hashpump
-brew install hydra
-brew install john
-brew install knock
-brew install nmap
-brew install pngcheck
-brew install socat
-brew install sqlmap
-brew install tcpflow
-brew install tcpreplay
-brew install tcptrace
-brew install ucspi-tcp # `tcpserver` etc.
-brew install xpdf
-brew install xz
-
-# Install other useful binaries.
-brew install ack
-#brew install exiv2
-brew install git
 brew install imagemagick --with-webp
-brew install lynx
-brew install p7zip
-brew install pigz
-brew install pv
-brew install rename
-brew install rhino
-brew install tree
-brew install webkit2png
-brew install zopfli
 
-# Install Node.js. Note: this installs `npm` too, using the recommended
-# installation method.
-brew install node
+apps=(
+  node
+  tree
+  wget
+  ack
+  docker
+  hub
+  git
+  mercurial
+)
 
-brew install homebrew/versions/lua52
+brew install ${apps[@]}
+
+# Cask apps
+brew install caskroom/cask/brew-cask
+apps=(
+  quicksilver
+  dropbox
+  google-chrome
+  transmit
+  appcleaner
+  firefox
+  seil
+  karabiner
+  spotify
+  sonos
+  vagrant
+  boot2docker
+  iterm2
+  virtualbox
+  sourcetree
+  nvalt
+  skype
+  hipchat
+)
+
+vagrant plugin install vagrant-bindfs
+
+# Install apps to /Applications
+# Default is: /Users/$user/Applications
+echo "installing apps..."
+brew cask install --appdir="/Applications" ${apps[@]}
 
 # Remove outdated versions from the cellar.
 brew cleanup
+
+brew linkapps
